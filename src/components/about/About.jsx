3dc062d7a87f5c9d7d28ddf3bc2bddf88db49c91
Dharmendra <dharmendraprajapati9503@gmail.com>
2023-03-19 11:02:38 +0530

    about updated

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -14,12 +14,14 @@ const About = () => {
                     <img src={AboutImg} alt="" className="about__img" />
 
                     <div className="about__data">
-
                         <p className="about__description">
-                            I’m Dharmendra Prajapati, a pre-final year student, pursuing B.Tech in Information Technology from Chandubhai S Patel Institute of Technology, Anand. My area of Interest are Data Structure and Algorithm, Software Development in Java, Frontend Development in React and Competitive Programming. I have brief knowledge about Spring Boot, MySQL but I don’t want to bound my knowledge and I want to keep refresh my knowledge.
+                            Hello, I’m Dharmendra Prajapati, a pre-final year student, pursuing <b> B.Tech </b> in <b> Information Technology </b> from <b> Chandubhai S Patel Institute of Technology, Anand. </b> 
+                            <br />
+                            <br />
+                            My area of Interest are Data Structure and Algorithm, Software Development in Java, Frontend Development in React and Competitive Programming. I have brief knowledge about Spring Boot, MySQL but I don’t want to bound my knowledge and I want to keep refresh my knowledge.
                         </p>
 
-                        <a download="" href={Resume} className="button button--flex">
+                        <a download="" href={Resume} className="dResume button button--flex">
                             Download Resume
 
                             <svg
